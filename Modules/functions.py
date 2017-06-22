be78ec3948784a41be2a835e1abe71e94b33e6d4
ballanr <ballanr@wwu.edu>
2017-06-22 14:03:15 -0700

    Add files via upload

--- Modules/functions.py
+++ Modules/functions.py
@@ -107,21 +107,10 @@ def Brackett_Ratios(plateid,mjd,fiber):
     import matplotlib.pyplot as plt
     import numpy as np
 
-    #Creating file path------------------------------------------------------------------------------------------------------------------------------|
-
-    server = '/Volumes/CoveyData/APOGEE_Spectra/python_DR13/dr13/apogee/spectro/redux/r6/apo25m/'
-    plate = str(plateid)
-    MJD = str(mjd)
-    fiber_num = str(fiber)
-    therest = 'apVisit-r6-'
-    dashes = '-'
-    f = '.fits'
-
-    endname = therest + plate + dashes + MJD + dashes + fiber_num + f
-    filename = os.path.join(server,plate,MJD,endname)
     
     #Header------------------------------------------------------------------------------------------------------------------------------------------|
 
+    filename = File_Path(plateid,mjd,fiber)
     main_header = fits.open(filename)
     
     loc_id = main_header[0].header['LOCID']
@@ -147,22 +136,11 @@ def Brackett_Ratios(plateid,mjd,fiber):
                     num = 11 + i
                     br = 'Br' + str(num) + ' Avg EqW'
                     br_value[i] = float(row[br])
-                '''
-                br_value.append([float(row['Br11 Avg EqW']),
-                            float(row['Br12 Avg EqW']),float(row['Br13 Avg EqW']),
-                            float(row['Br14 Avg EqW']),float(row['Br15 Avg EqW']),
-                            float(row['Br16 Avg EqW']),float(row['Br17 Avg EqW']),
-                            float(row['Br18 Avg EqW']),float(row['Br19 Avg EqW']),
-                            float(row['Br20 Avg EqW'])])
-                '''
-    print(br_value)
+
     #Plotting---------------------------------------------------------------------------------------------------------------------------------------|    
-    
-    
     
     fig,ax=plt.subplots(figsize=(16,8))
     ax.tick_params(axis='both', labelsize=20)
-    
     
     plt.plot(br_num,br_value/br_value[0])
     plt.ylabel('Br n>11 / Br 11',fontsize=24)
@@ -170,3 +148,230 @@ def Brackett_Ratios(plateid,mjd,fiber):
     plt.show()
     
 
+def File_Path(plateid,mjd,fiber):
+
+    import os
+
+    #Creating file path------------------------------------------------------------------------------------------------------------------------------|
+
+    server = '/Volumes/CoveyData/APOGEE_Spectra/python_DR13/dr13/apogee/spectro/redux/r6/apo25m/'
+    plate = str(plateid)
+    MJD = str(mjd)
+    fiber_num = str(fiber)
+    therest = 'apVisit-r6-'
+    dashes = '-'
+    f = '.fits'
+
+    endname = therest + plate + dashes + MJD + dashes + fiber_num + f
+    filename = os.path.join(server,plate,MJD,endname)
+
+    return filename
+    
+def Balmer_Decrement_Plot():
+
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    #Importing Decrement File------------------------------------------------------------------------------------------------------------------------|
+    data = np.loadtxt('/Users/ballanr/Desktop/Fwd__Bracket_Decrement/x74.txt',delimiter = '\t',unpack=True)
+    print(data[0])
+    T = [3750,5000,7500,8750,10000,12500,15000]
+    for i in range(len(T)):
+        plt.plot(np.linspace(8,12.4,num=23),data[i],label = str(T[i]) + ' K')
+        plt.scatter(np.linspace(8,12.4,num=23),data[i])
+    plt.xticks(np.arange(8,12.6,0.2))
+    for k in range(5):
+        x = 8 + k
+        plt.axvline(x,ls = 'dashed',linewidth = 0.5,color = 'black')
+        g = 0.1 + (0.1*k)
+        plt.axhline(g,ls = 'dashed', linewidth = 0.5, color = 'black')
+    plt.xlabel('Log $n_e$')
+    plt.ylabel('Transition Probabilities')
+    plt.legend()
+    plt.show()
+    
+def Probs_Calc(n,T):
+
+    import numpy as np
+
+    #Constants------------------------------------------------------------------------------------------------------------------------|
+    k = 8.6173303*(10**(-5)) #eV/K
+   
+    #Calculations---------------------------------------------------------------------------------------------------------------------|
+    n_i = 4**2
+    n_k = n**2
+    E_1 = -13.6/n_i
+    E_2 = -13.6/n_k
+    exponent = -(E_2 - E_1)/(k*T)
+    boltz = 2*(n_k/n_i)*np.exp(exponent)
+
+    return boltz
+
+def Probs_Plots():
+    import numpy as np
+    import functions
+    import matplotlib.pyplot as plt
+    
+    T = [3750,5000,7500,8750,10000,12500,15000]
+    for j in range(len(T)):
+        xx = []
+        yy = []
+        for i in range(20):
+            x = 1 + i
+            y = functions.Probs_Calc(x,T[j])
+            y = np.log(y)
+            #plt.scatter(x,y)
+            xx.append(x)
+            yy.append(y)
+        plt.plot(xx,yy,label = str(T[j]) + ' K')
+        plt.scatter(xx,yy)
+        plt.xlabel('N')
+        plt.ylabel('Probability')
+    plt.legend(bbox_to_anchor=(1.25,1))
+    #plt.xlim(2,20)
+    plt.xticks(np.arange(2,21,1))
+    #plt.ylim(-2.75,1)
+    plt.show()
+
+def Saha(n,T,density):
+
+    '''
+    This isn't really useful right now...this looks at the ionization states,
+    not the transition probabilities...
+    '''
+
+    import numpy as np
+
+    k_J = 1.380648*(10**(-23)) #Joules
+    k_e = 8.617*(10**(-5)) # eVs
+    
+    n_i = n**2
+    n_k = (n+1)**2
+    n_e = density
+    h = 6.626*(10**(-34))
+    m_e = 9.109*(10**(-31))
+    E_1 = -13.6/n_i
+    E_2 = -13.6/n_k
+    E = E_2 - E_1
+
+    part1 = (1/n_e)
+    #print(part1)
+    part2 = (2*(n_k/n_i))
+    part2 = 1
+    #print(part2)
+    part3 = (((2*np.pi*m_e*k_J*T)/(h**2))**(3/2))
+    #print(part3)
+    part4 = (np.exp(E_1/(k_e*T)))
+    #print(part4)
+    
+    #A = np.log(part1*part2*part3*part4)
+    A = part1*part2*part3*part4
+    
+    return A
+    
+    
+def Boltzmann(n_upper,n_lower,T):
+    
+    import numpy as np
+
+    k_e = 8.617*(10**(-5)) # eVs
+    
+
+    g_b = 2*(n_upper**2)
+    g_a = 2*((n_lower)**2)
+    del_E = -3.4+13.6
+    E_b = -13.6/(n_upper**2)
+    E_a = -13.6/(n_lower**2)
+
+    B = (g_b / g_a)*np.exp(-(E_b - E_a)/(k_e * T))
+
+    return B
+
+def Saha_Boltzmann(n_upper,n_lower,ion,T,density):
+    
+    import functions
+    import numpy as np
+
+    x = functions.Saha(ion,T,density)
+    y = functions.Boltzmann(n_upper,n_lower,T)
+    
+    SB = (y/(1+y))*(1/(1+x))
+
+    return np.log(SB)
+
+def SB_Plotter():
+
+    import functions
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    T = [3750,5000,7500,8750,10000,12500,15000]
+    gg = [11,12,13,14,15,16,17,18,19,20]
+    densities = []
+    for i in range(23):
+        y = 8 + (0.2*i)
+        x = (100**3)*(np.exp(y))
+        densities.append(x)
+    #Plotter 1
+    '''
+    yy = []
+    for i in range(len(T)):
+        y = np.log(functions.Saha_Boltzmann(11,4,1,T[i]))
+        yy.append(y)
+    plt.plot(T,yy)
+    plt.scatter(T,yy)
+    '''
+    #Plotter 2
+
+    for i in range(len(T)):
+        
+        for j in range(len(densities)):
+            yy = []
+            for k in range(len(gg)):
+                y = functions.Saha_Boltzmann(gg[k],4,1,T[i],densities[j])
+                yy.append(y)
+            plt.plot(gg,yy,label='Temp ' + str(T[i])+'density '+str(densities[j]))
+            plt.scatter(gg,yy)
+    plt.legend(bbox_to_anchor=(1,1))
+    plt.show()
+
+def SB_CSV(savefile):
+
+    import csv
+    import functions
+    import numpy as np
+    import matplotlib.pyplot as plt
+    
+    with open(savefile,'w') as savefile:
+
+        densities = []
+        for i in range(23):
+            y = 8 + (0.2*i)
+            x = (100**3)*(np.exp(y))
+            densities.append(x)
+        
+        T = [3750,5000,7500,8750,10000,12500,15000]
+        brackett = [11,12,13,14,15,16,17,18,19,20]
+
+        names = ['Densities','3750','5000','7500','8750','10000','12500','15000']
+        writer = csv.DictWriter(savefile,delimiter = '\t',fieldnames = names)
+        writer.writeheader()
+        b=[]
+        for i in range(len(densities)):
+            a = []
+            for k in range(len(T)):
+                y = functions.Saha_Boltzmann(11,4,1,T[k],densities[i])
+                a.append(y)
+            #writer.writerow({'Densities':densities[i],'3750':a[0],'5000':a[1],'7500':a[2],'8750':a[3],'10000':a[4],'12500':a[5],'15000':a[6]})
+            b.append(a)
+        for i in range(len(b)):
+            for k in range(7):
+                b[i][k] = b[i][k]/b[12][3]
+        for i in range(len(densities)):
+            writer.writerow({'Densities':densities[i],'3750':b[i][0],'5000':b[i][1],'7500':b[i][2],'8750':b[i][3],'10000':b[i][4],'12500':b[i][5],'15000':b[i][6]})
+
+        for i in range(23):
+            plt.plot(T,b[i])
+            plt.scatter(T,b[i])
+        plt.legend(bbox_to_anchor=(1,1))
+        plt.show()
--- Modules/testing.py
+++ Modules/testing.py
@@ -1,10 +1,17 @@
 #Testing of modules done here
 
-import Functions
+import functions
 
 #x = Functions.Br_Equiv_Width(6218,56168,148,11)
 #print(x)
 
-x = Functions.Brackett_Ratios(6218,56168,148)
+#x = Functions.Brackett_Ratios(6218,56168,148)
+#x = functions.Balmer_Decrement_Plot()
+#g = functions.Probs_Calc(11,3750)
+#g = functions.Probs_Plots()
+#g = functions.Saha_Boltzmann(11,4,1,3750,2980957987.04)
+#g = functions.Saha(1)
+#print(g)
 
-
+g = functions.SB_Plotter()
+#g = functions.SB_CSV('test3.csv')
